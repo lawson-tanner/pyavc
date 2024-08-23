d808fb8963d52f4b80f2e9196d41921d4a1a7e39
Lawson Tanner <107106831+lawson-tanner@users.noreply.github.com>
2024-08-24 00:51:37 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="your-package-name",
+    name="pyavc",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="A short description of your package",
+    author="Lawson Tanner",
+    author_email="pyavc@lawsonia.cc",
+    description="A command line utility and library for converting DOCX and TXT files to Avid Script Files (.avc)",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/your-package-name",
+    url="https://github.com/lawson-tanner/pyavc",
     packages=find_packages(),  # Automatically find and include packages
     classifiers=[
         "Programming Language :: Python :: 3",
