063ca04e9ab47cf2bbae394bb26561b3da325c7d
Lawson Tanner <107106831+lawson-tanner@users.noreply.github.com>
2024-08-28 16:48:55 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyavc",
-    version="1.0.10",
+    version="1.0.11",
     author="Lawson Tanner",
     author_email="pyavc@lawsonia.cc",
     description="A command line utility and library for converting DOCX and TXT files to Avid Script Files (.avc)",
